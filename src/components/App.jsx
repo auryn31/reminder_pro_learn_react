448bd5fbe945ce0644005891cbb9dcbf229551dc
auryn <auryn31@icloud.com>
2017-10-18 17:34:16 +0200

    add css and list of items from global stats: https://www.youtube.com/watch?v=oCAc3EsEWZ8

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,23 @@ class App extends Component {
       this.props.addReminder(this.state.text);
     }
 
+    renderReminders(){
+      const  { reminders } = this.props;
+      console.log('reminders', reminders);
+      return (
+        <ul className="list-group col-sm-4">
+            {
+              reminders.map(reminder => {
+                return (<li key={reminder.id} className="list-group-item">
+                  <div>{reminder.text}</div>
+                </li>
+              )
+              })
+            }
+        </ul>
+      )
+    }
+
     render() {
       console.log('this.props', this.props);
       return (
@@ -22,7 +39,7 @@ class App extends Component {
           <div className="title">
               Reminder Pro
           </div>
-          <div className ="form-inline">
+          <div className ="form-inline reminder-form">
             <div className ="form-group">
               <input
                 className="form-control"
@@ -37,6 +54,7 @@ class App extends Component {
               Add Reminder
             </button>
           </div>
+          { this.renderReminders() }
         </div>
       )
     }
