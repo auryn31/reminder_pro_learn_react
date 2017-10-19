6324399898ccc7660d4a52c34e9de38a7963f785
auryn <auryn31@icloud.com>
2017-10-19 10:47:15 +0200

    add cookie to store reminders in browser

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies';
 
 
 const reminder = (action) => {
@@ -19,13 +20,15 @@ const removeByID = (state = [], id) => {
 
 const reminders = (state = [], action) => {
   let reminders = null;
+  state = read_cookie('reminders');
   switch (action.type) {
     case ADD_REMINDER:
         reminders = [...state, reminder(action)]
-        console.log('reminder as state', reminders);
+        bake_cookie('reminders', reminders);
         return reminders;
     case DELETE_REMINDER:
         reminders = removeByID(state, action.id);
+        bake_cookie('reminders', reminders);
         return reminders;
     default:
     return state;
